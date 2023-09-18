69f73f98824cdd85546e374af616ff263e5de250
quaiserchen <113685733+quaiserchen@users.noreply.github.com>
2023-09-18 15:15:40 +0200

    Update bruch.h

--- 3_TBruch/TBruch/bruch.h
+++ 3_TBruch/TBruch/bruch.h
@@ -1,5 +1,7 @@
 //Klasse TBruch
 
+#include <string>
+
 class CBruch{
 
 private:
@@ -17,16 +19,45 @@ public:
     //Konstruktor mit 2 Werten
     CBruch(long z, long n);
 
-   //Konstruktor f�r Double-Werte
+   //Konstruktor für Double-Werte
    CBruch(double d);
 
-   //Funktion zum k�rzen
+   //Funktion zum kürzen
    void kuerze();
 
-   void getBruch();
+   // Funktion zur bestimmung des gemeinsamen Teilers
+   int ggt(long z, long n);
 
+   // Gibt den Bruch aus
+   std::string getBruch();
+
+   // zum Überladen der Operatoren, wenn mit zwei CBruch-Klassen gearbeited wird
    CBruch operator+(CBruch Bruch);
    CBruch operator-(CBruch Bruch);
    CBruch operator*(CBruch Bruch);
    CBruch operator/(CBruch Bruch);
+
+   // überschreibt Operatoren für CBruch -> +-*/ -> double
+   CBruch operator+(double d);
+   CBruch operator-(double d);
+   CBruch operator*(double d);
+   CBruch operator/(double d);
+
+   // überschreibt Operatoren für CBruch -> +-*/ -> long
+   CBruch operator+(long l);
+   CBruch operator-(long l);
+   CBruch operator*(long l);
+   CBruch operator/(long l);
+
 };
+    // überschreibt Operatoren für double -> +-*/ -> CBruch
+    CBruch operator+(double d, const CBruch &bruch);
+    CBruch operator-(double d, const CBruch &bruch);
+    CBruch operator*(double d, const CBruch &bruch);
+    CBruch operator/(double d, const CBruch &bruch);
+
+// überschreibt Operatoren für double -> +-*/ -> CBruch
+    CBruch operator+(long l, const CBruch &bruch);
+    CBruch operator-(long l, const CBruch &bruch);
+    CBruch operator*(long l, const CBruch &bruch);
+    CBruch operator/(long l, const CBruch &bruch);
